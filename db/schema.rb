8ecab2d334c11036fb10468461cfe1f20d987d9e
Ngoc Hoang <hoangcnpd01980@fpt.edu.vn>
2019-08-15 11:17:01 +0700

    183: Create model Bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_15_041233) do
+ActiveRecord::Schema.define(version: 2019_08_15_041614) do
+
+  create_table "bookings", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "checkin"
+    t.datetime "checkout"
+    t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -2,6 +2,6 @@
 
 require "rails_helper"
 
-RSpec.describe Room, type: :model do
+RSpec.describe Booking, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
