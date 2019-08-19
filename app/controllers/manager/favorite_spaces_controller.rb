495aba546a4400aeda0e6cff8f263f5ce1058a8e
Ngoc Hoang <hoangcnpd01980@fpt.edu.vn>
2019-08-19 10:44:23 +0700

    Create price

--- app/controllers/manager/favorite_spaces_controller.rb
+++ app/controllers/manager/favorite_spaces_controller.rb
@@ -2,7 +2,7 @@
 
 module Manager
   class FavoriteSpacesController < BaseController
-    before_action :favorite_space, only: %i[edit update]
+    before_action :favorite_space, only: %i[edit update destroy]
 
     def index
       @favorite_spaces = FavoriteSpace.order("created_at DESC")
