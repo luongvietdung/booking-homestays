6f02f735f72bf24e186f3ca1001e8dd654e13fa5
Ngoc Hoang <hoangcnpd01980@fpt.edu.vn>
2019-08-19 10:44:23 +0700

    Update price

--- app/controllers/manager/prices_controller.rb
+++ app/controllers/manager/prices_controller.rb
@@ -2,6 +2,8 @@
 
 module Manager
   class PricesController < BaseController
+    before_action :price, only: %i[edit update]
+
     def index
       @prices = Price.newest
     end
@@ -19,10 +21,24 @@ module Manager
       end
     end
 
+    def edit; end
+
+    def update
+      if @price.update(price_params)
+        redirect_to manager_prices_path, flash: { success: t(".success") }
+      else
+        render :edit
+      end
+    end
+
     private
 
     def price_params
       params.require(:price).permit(:cost, :cleaning_fee)
+    end
+
+    def price
+      @price = Price.find(params[:id])
     end
   end
 end
