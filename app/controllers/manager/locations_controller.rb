741eceb9850054f9ad00ab599c95eaa75451c8d4
HieuCR7 <hieuhv97@gmail.com>
2019-08-19 14:05:08 +0700

    125: edit locations

--- app/controllers/manager/locations_controller.rb
+++ app/controllers/manager/locations_controller.rb
@@ -2,8 +2,22 @@
 
 module Manager
   class LocationsController < BaseController
+    before_action :load_location, only: %i[edit update]
+
     def new
       @location = Location.new
+    end
+
+    def edit; end
+
+    def update
+      if @location.update_attributes location_params
+        flash[:success] = t ".success"
+        redirect_to manager_locations_path
+      else
+        flash.now[:danger] = t ".danger"
+        render :edit
+      end
     end
 
     def create
@@ -12,7 +26,7 @@ module Manager
         flash[:success] = t ".success"
         redirect_to manager_locations_path
       else
-        flash[:danger] = t ".danger"
+        flash.now[:danger] = t ".danger"
         render :new
       end
     end
@@ -21,6 +35,10 @@ module Manager
 
     def location_params
       params.require(:location).permit :name
+    end
+
+    def load_location
+      @location = Location.find params[:id]
     end
   end
 end
