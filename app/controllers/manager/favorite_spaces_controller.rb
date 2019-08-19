fa6f7bde2eb26033cbbcd1e5a1e9a4e782462adf
Ngoc Hoang <hoangcnpd01980@fpt.edu.vn>
2019-08-19 09:07:07 +0700

    #129: Edit space favorites

--- app/controllers/manager/favorite_spaces_controller.rb
+++ app/controllers/manager/favorite_spaces_controller.rb
@@ -2,6 +2,7 @@
 
 module Manager
   class FavoriteSpacesController < BaseController
+    before_action :find_favorite_spaces, only: %i[edit update]
     def index
       @favorite_spaces = FavoriteSpace.order("created_at DESC")
     end
@@ -19,10 +20,24 @@ module Manager
       end
     end
 
+    def edit; end
+
+    def update
+      if @favorite_space.update_attributes(favorite_space_params)
+        redirect_to manager_favorite_spaces_path, flash: { success: t(".success") }
+      else
+        render :edit
+      end
+    end
+
     private
 
     def favorite_space_params
       params.require(:favorite_space).permit(:name)
+    end
+
+    def find_favorite_spaces
+      @favorite_space = FavoriteSpace.find(params[:id])
     end
   end
 end
