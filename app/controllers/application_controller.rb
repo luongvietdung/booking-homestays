9b3559abbbd6621dfac04f0c09e06f91e3bca5e1
huylc <conghuy1006@gmail.com>
2019-08-20 12:57:58 +0700

    198: edit admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    stored_location_for(resource) || manager_root_path if resource.type == "Admin"
+    stored_location_for(resource) || manager_admins_path if resource.type == "Admin"
   end
 
   def after_sign_out_path_for(resource_or_scope)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,21 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  attr_accessor :not_update_password
+
   devise :database_authenticatable, :recoverable, :rememberable, :validatable
   has_many :rooms
 
   validates :name, presence: true, length: { maximum: 45 }
   validates :address, presence: true, length: { maximum: 255 }
+  validates :type, presence: true
+  has_one_attached :avatar
+
+  protected
+
+  def password_required?
+    return false if not_update_password
+
+    super
+  end
 end
