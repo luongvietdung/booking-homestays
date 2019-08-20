033ae032385b628836922d6ea5d9c38fd080b2a0
Ngoc Hoang <hoangcnpd01980@fpt.edu.vn>
2019-08-20 14:37:40 +0700

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_16_042328) do
+ActiveRecord::Schema.define(version: 2019_08_19_032118) do
 
   create_table "favorite_spaces", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
