07ab82c28f3ab21267fc90fe5ca09fc99d4b092a
huylc <conghuy1006@gmail.com>
2019-09-02 20:51:00 +0700

    175: edit-profile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,20 +5,4 @@ module ApplicationHelper
     base_title = t ".title"
     page_title.empty? ? base_title : "#{page_title} | #{base_title}"
   end
-
-  def avatar_member(member)
-    if member.avatar?
-      image_tag member.avatar.url, class: "avatar-default-member", id: "img_member_prev"
-    else
-      image_tag "defaultavatar.jpeg", class: "avatar-default-member", id: "img_member_prev"
-    end
-  end
-
-  def avatar_admin(admin)
-    if admin.avatar?
-      image_tag admin.avatar.url, class: "avatar-default", id: "img_prev"
-    else
-      image_tag "defaultavatar.jpeg", class: "avatar-default", id: "img_prev"
-    end
-  end
 end
